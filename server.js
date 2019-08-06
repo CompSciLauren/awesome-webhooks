c44409790255378c1ba20c8db296c6e9e7c7b435
Lauren Stephenson <compscilauren@gmail.com>
2019-08-06 18:37:00 -0500

    Fix calls to all function correctly

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+let express = require('express');
+let bodyParser = require('body-parser');
+let axios = require('axios');
 
-const app = express();
+let app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const port = 3000;
+let port = 3000;
 
-const fileToInspect = 'pom.xml';
-const feature = '<enabled>true</enabled>';
+let fileToInspect = 'server.js'; //fixme to pom.xml
+let feature = 'express'; //fixme to <enabled>true</enabled>
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -18,57 +18,72 @@ app.get('/', (req, res) => {
 app.post('/payload', async (req, res) => {
   switch (req.body.action) {
     case 'opened':
+    case 'closed':
     case 'reopened':
     case 'synchronize':
       console.log('Pull request action: ' + req.body.action);
-      const commitsUrl = req.body.pull_request.commits_url;
-      const patchUrl = await axios
+      let commitsUrl = req.body.pull_request.commits_url;
+      let patchUrl = await axios
         .get(commitsUrl)
         .then(result => result.data[0])
-        .then(pullRequest => pullRequest.url)
+        .then(pullRequest => {
+          console.log('patchUrl: ' + pullRequest.url);
+          return pullRequest.url;
+        })
         .catch(error => {
-          console.log(error);
+          console.log('patchUrl Error: ' + error);
         });
 
-      const individualCommitsUrl = await axios
+      let individualCommitsUrl = await axios
         .get(patchUrl)
         .then(result => result.data)
-        .then(pullRequest => pullRequest.url)
+        .then(pullRequest => {
+          console.log('individualCommitsUrl: ' + pullRequest.url);
+          return pullRequest.url;
+        })
         .catch(error => {
-          console.log(error);
+          console.log('individualCommitsUrl Error: ' + error);
         });
 
-      const chosenCommitUrl = await axios
+      let chosenCommitUrl = await axios
         .get(individualCommitsUrl)
         .then(result => result.data)
         .then(pullRequest => {
           for (let i = 0; i < pullRequest.files.length; i++) {
             if (pullRequest.files[i].filename == fileToInspect) {
+              console.log('chosenCommitUrl: ' + pullRequest.files[0].raw_url);
               return pullRequest.files[0].raw_url;
             }
           }
-          return 'Error: ' + fileToInspect + ' file not found.';
+          return (
+            'Error: ' +
+            fileToInspect +
+            ' file not found.' +
+            pullRequest.files[0].raw_url
+          );
         })
         .catch(error => {
           console.log(error);
         });
 
-      const rawCodeUrl = await axios
+      let rawCodeUrl = await axios
         .get(chosenCommitUrl)
         .then(result => result.data)
         .then(pullRequest => {
-          const isSnapshotEnabled = pullRequest.includes(feature);
+          let isSnapshotEnabled = pullRequest.includes(feature);
           if (isSnapshotEnabled == true) {
             isSnapshotEnabled = 'feature exists';
           } else {
             isSnapshotEnabled = 'feature does not exist';
           }
-          console.log(pullRequest);
+          console.log('//////////////////////////////////////////////////////');
+          console.log('Pull Request: \n' + pullRequest);
+          console.log('//////////////////////////////////////////////////////');
           console.log('Feature status: ' + isSnapshotEnabled);
           return pullRequest;
         })
         .catch(error => {
-          console.log(error);
+          console.log('Error: ' + error);
         });
   }
   res.json({ hello: 'world' });

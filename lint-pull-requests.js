133a5b94fa832e21f846c9dcf9b1dcbd2e251188
Lauren Stephenson <compscilauren@gmail.com>
2019-08-24 10:40:35 -0500

    Progress

--- lint-pull-requests.js
+++ lint-pull-requests.js
@@ -1,3 +1,15 @@
+/**
+ * How to use this webhook:
+ * 1. Go to your project on GitHub > Settings > Webhooks & services > Add Webhook
+ * 2. Payload URL: (https://....com/)
+ * 3. Content type: `application/json`
+ * 4. Secret: Leave blank
+ * 5. Let me select individual events > Check `Pull Request`
+ * 6. Add webhook and you are done.
+ *
+ * Next time a pull request is opened, a bot will comment on the linter status.
+ */
+
 let express = require('express');
 let bodyParser = require('body-parser');
 
@@ -14,9 +26,26 @@ app.get('/', (req, res) => {
 app.post('/payload', async (req, res) => {
   switch (req.body.action) {
     case 'opened':
-    // run linter
+      // lint
+      commentOnPullRequest();
   }
   res.json({ hello: 'world' });
 });
+
+function commentOnPullRequest() {
+  const privateToken = process.env.OAUTH_TOKEN;
+  const projectId = process.env.CI_PROJECT_ID;
+  const mergeRequestId = process.env.PR_ID;
+  const message = `Message here.`;
+
+  const path =
+    'https://gitlab.com/api/v4/projects/' +
+    projectId +
+    '/merge_requests/' +
+    mergeRequestId +
+    '/notes?private_token=' +
+    privateToken;
+  request.post(path, { form: { body: message } });
+}
 
 app.listen(port, () => console.log(`This app is listening on port ${port}!`));
